e611e42d70250772c94c35095ca3e82e23359023
Dennis Rutherford <dennis@rutherford.us>
2023-03-07 08:41:05 -0500

    Removed duplicate function 'set-apiparameters'

--- functions/Set-DrmmApiParameters.ps1
+++ functions/Set-DrmmApiParameters.ps1
@@ -6,29 +6,29 @@ function Set-DrmmApiParameters {
 	.PARAMETER Url
 	Provide Datto RMM API Url. See Datto RMM API help files for more information.
 
-	.PARAMETER Key
-	Provide Dattto RMM API Key. Obtained when creating a API user in Datto RMM.
-
-	.PARAMETER SecretKey
-	Provide Datto RMM API ScretKey. Obtained when creating a API user in Datto RMM.
-	
+	.PARAMETER PublicPrivateKey
+	Provide Dattto RMM API Key as user and Datto RMM API SecretKey as password. Obtained when creating a API user in Datto RMM.
 	#>
-	
+
+	[CmdletBinding()]
 	Param(
-	[Parameter(Mandatory=$True)]
+	[Parameter(Position = 0, Mandatory=$True)]
+	[ValidateSet(
+		"https://pinotage-api.centrastage.net",
+		"https://merlot-api.centrastage.net",
+		"https://concord-api.centrastage.net",
+		"https://zinfandel-api.centrastage.net",
+		"https://syrah-api.centrastage.net"
+	)]
 	$Url,
     
-	[Parameter(Mandatory=$True)]
-	$Key,
+	[Parameter(Position = 1, Mandatory=$True)]
+	[PScredential]$PublicPrivateKey
 
-	[Parameter(Mandatory=$True)]
-	$SecretKey
-	
 	)
 
 	New-Variable -Name apiUrl -Value $Url -Scope Script -Force
-	New-Variable -Name apiKey -Value $Key -Scope Script -Force
-	New-Variable -Name apiSecretKey -Value $SecretKey -Scope Script -Force
+	New-Variable -Name PublicPrivateKey -Value $PublicPrivateKey -Scope Script -Force
 	
 	$accessToken = New-ApiAccessToken
 	New-Variable -Name apiAccessToken -value $accessToken -Scope Script -Force
