1b5895e8a1ffae5c0d70dc05037f02134f0c4c44
Dennis Rutherford <dennis@rutherford.us>
2023-03-07 08:08:16 -0500

    Changed build to separate directory

--- BuildModule.ps1
+++ BuildModule.ps1
@@ -1,8 +1,8 @@
-New-Item -ItemType Directory -Path $PSScriptroot\Development\DattoRMM -ErrorAction SilentlyContinue -Force
-Remove-Item $PSScriptroot\Development\DattoRMM\DattoRMM.psm1 -ErrorAction SilentlyContinue -Force
-Remove-Item $PSScriptroot\Development\DattoRMM\DattoRMM.psd1 -ErrorAction SilentlyContinue -Force
-Remove-Item $PSScriptroot\Development\DattoRMM\DattoRMM.ps1 -ErrorAction SilentlyContinue -Force
-Get-ChildItem -Recurse .\functions\*.ps1 |ForEach-Object{ Get-Content $_ |out-file -FilePath .\Development\DattoRMM\DattoRMM.psm1 -Append} 
+New-Item -ItemType Directory -Path $PSScriptroot\Build\DattoRMM -ErrorAction SilentlyContinue -Force | Out-Null
+Remove-Item $PSScriptroot\Build\DattoRMM\DattoRMM.psm1 -ErrorAction SilentlyContinue -Force
+Remove-Item $PSScriptroot\Build\DattoRMM\DattoRMM.psd1 -ErrorAction SilentlyContinue -Force
+Remove-Item $PSScriptroot\Build\DattoRMM\DattoRMM.ps1 -ErrorAction SilentlyContinue -Force
+Get-ChildItem -Recurse .\functions\*.ps1 |ForEach-Object{ Get-Content $_ |out-file -FilePath .\Build\DattoRMM\DattoRMM.psm1 -Append} 
 $PublicFunctions = Get-ChildItem -Recurse .\functions\*.ps1
-Copy-Item $PSScriptroot\DattoRMM.psd1 $PSScriptroot\Development\DattoRMM\DattoRMM.psd1
-Update-ModuleManifest .\Development\DattoRMM\DattoRMM.psd1 -FunctionsToExport $PublicFunctions.basename
+Copy-Item $PSScriptroot\DattoRMM.psd1 $PSScriptroot\Build\DattoRMM\DattoRMM.psd1
+Update-ModuleManifest .\Build\DattoRMM\DattoRMM.psd1 -FunctionsToExport $PublicFunctions.basename
