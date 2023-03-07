ad4b68408c7dcec5cd6cc6542314703eaf27c7f6
Dennis Rutherford <dennis@rutherford.us>
2023-03-07 08:54:33 -0500

    Removed 'Drmm' from all modules. Added Drmm as default prefix for module. Split out private functions.

--- BuildModule.ps1
+++ BuildModule.ps1
@@ -2,7 +2,7 @@ New-Item -ItemType Directory -Path $PSScriptroot\Build\DattoRMM -ErrorAction Sil
 Remove-Item $PSScriptroot\Build\DattoRMM\DattoRMM.psm1 -ErrorAction SilentlyContinue -Force
 Remove-Item $PSScriptroot\Build\DattoRMM\DattoRMM.psd1 -ErrorAction SilentlyContinue -Force
 Remove-Item $PSScriptroot\Build\DattoRMM\DattoRMM.ps1 -ErrorAction SilentlyContinue -Force
-Get-ChildItem -Recurse .\functions\*.ps1 |ForEach-Object{ Get-Content $_ |out-file -FilePath .\Build\DattoRMM\DattoRMM.psm1 -Append} 
-$PublicFunctions = Get-ChildItem -Recurse .\functions\*.ps1
+Get-ChildItem -Recurse .\functions\*.ps1 |ForEach-Object{ Get-Content $_ |out-file -FilePath .\Build\DattoRMM\DattoRMM.psm1 -Append } 
+$PublicFunctions = Get-ChildItem -Recurse .\functions\Public\*.ps1
 Copy-Item $PSScriptroot\DattoRMM.psd1 $PSScriptroot\Build\DattoRMM\DattoRMM.psd1
 Update-ModuleManifest .\Build\DattoRMM\DattoRMM.psd1 -FunctionsToExport $PublicFunctions.basename
